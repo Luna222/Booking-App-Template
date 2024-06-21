222c21488edf74130cb97a4668b236c65bcc111d
Luna222 <lunameowth@gmail.com>
2024-06-21 19:55:06 +0700

    up

--- src/components/Header/SearchBox.jsx
+++ src/components/Header/SearchBox.jsx
@@ -49,10 +49,19 @@ const SearchBox = () => {
             />
           </div>
 
-          <div className="flex grow shrink basis-auto items-center flex-nowrap border rounded-bl rounded-br tablet:rounded-r tablet:rounded-bl-none p-2 bg-white text-gray-600 whitespace-nowrap hocus:border-2 hocus:border-rose-600">
-            <i className="fa fa-female pl-2 py-2"></i>
-            <button className="outline-none bg-transparent p-2 placeholder-gray-400">
-              2 adults · 0 children · 1 room
+          <div className="grow shrink basis-auto border rounded-bl rounded-br tablet:rounded-r tablet:rounded-bl-none p-2 bg-white text-gray-600 whitespace-nowrap hocus:border-2 hocus:border-rose-600">
+            <button className="flex justify-between items-center flex-nowrap w-full outline-none bg-transparent p-2 placeholder-gray-400">
+              <div>
+                <i className="fa fa-female pr-2"></i>
+                <span>2 adults · 0 children · 1 room</span>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="w-4 h-full"
+              >
+                <path d="M19.268 8.913a.9.9 0 0 1-.266.642l-6.057 6.057A1.3 1.3 0 0 1 12 16c-.35.008-.69-.123-.945-.364L4.998 9.58a.91.91 0 0 1 0-1.284.897.897 0 0 1 1.284 0L12 13.99l5.718-5.718a.897.897 0 0 1 1.284 0 .88.88 0 0 1 .266.642"></path>
+              </svg>
             </button>
           </div>
 
