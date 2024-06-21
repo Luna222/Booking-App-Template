31afa2dd279df7bcb1b358b90ae92833d68d9577
Luna222 <lunameowth@gmail.com>
2024-06-21 14:22:45 +0700

    up

--- src/components/Header/SearchBox.jsx
+++ src/components/Header/SearchBox.jsx
@@ -15,46 +15,48 @@ const SearchBox = () => {
 
   return (
     <div className="container-md max-w-[1100px] tablet:w-full tablet:absolute tablet:left-[50%] tablet:translate-x-[-50%] tablet:translate-y-[-135px] tablet:z-[1] overflow-visible">
-      <form action="" className="h-max">
+      <form action="">
         <div className="flex flex-col tablet:flex-row tablet:justify-center tablet:items-center flex-wrap desktop:flex-nowrap p-1 bg-[#FFB700] rounded-md shadow-md mt-6 mb-4">
-          <div className="flex grow shrink basis-auto items-center flex-nowrap border rounded-tl rounded-tr tablet:rounded-l tablet:rounded-tr-none p-4 bg-white text-gray-600 whitespace-nowrap">
-            <i className="fa fa-bed mr-2"></i>
+          {/* control */}
+          <div className="flex grow shrink basis-auto items-center flex-nowrap border rounded-tl rounded-tr tablet:rounded-l tablet:rounded-tr-none p-2 bg-white text-gray-600 whitespace-nowrap">
+            <i className="fa fa-bed pl-2 py-2"></i>
             <input
               type="text"
               placeholder="Where are you going?"
-              className="outline-none bg-transparent placeholder-gray-400"
+              className="outline-none bg-transparent p-2 placeholder-gray-400"
             />
           </div>
 
-          <div className="flex grow shrink basis-auto items-center flex-nowrap border p-4 bg-white text-gray-600 relative z-[2] whitespace-nowrap">
-            <i className="fa fa-calendar mr-2"></i>
+          <div className="flex grow shrink basis-auto items-center flex-nowrap border p-2 bg-white text-gray-600 z-[2] relative whitespace-nowrap">
+            <i className="fa fa-calendar pl-2 py-2"></i>
             {/* <DateRange
               editableDateInputs={true}
               onChange={item => setState([item.selection])}
               moveRangeOnFirstSelection={false}
               ranges={state}
-              className="w-full tablet:w-[calc(1100px / 3)] absolute top-full left-0 z-[3]"
+              className="tablet:w-[calc(1100px / 3)] absolute top-full left-0 shadow-lg z-[3]"
             /> */}
             <input
               type="text"
               placeholder="Check-in date"
-              className="outline-none bg-transparent placeholder-gray-400"
+              className="tablet:w-[130px] outline-none bg-transparent p-2 placeholder-gray-400"
             />
-            <span className="mx-2">—</span>
+            <span className="text-gray-400">—</span>
             <input
               type="text"
               placeholder="Check-out date"
-              className="outline-none bg-transparent placeholder-gray-400"
+              className="tablet:w-[130px] outline-none bg-transparent p-2 placeholder-gray-400"
             />
           </div>
 
-          <div className="flex grow shrink basis-auto items-center flex-nowrap border rounded-bl rounded-br tablet:rounded-r tablet:rounded-bl-none p-4 bg-white text-gray-600 whitespace-nowrap">
-            <i className="fa fa-female mr-2"></i>
-            <button className="outline-none bg-transparent placeholder-gray-400">
+          <div className="flex grow shrink basis-auto items-center flex-nowrap border rounded-bl rounded-br tablet:rounded-r tablet:rounded-bl-none p-2 bg-white text-gray-600 whitespace-nowrap">
+            <i className="fa fa-female pl-2 py-2"></i>
+            <button className="outline-none bg-transparent p-2 placeholder-gray-400">
               2 adults · 0 children · 1 room
             </button>
           </div>
 
+          {/* action */}
           <button className="primaryBtn grow shrink p-4 mt-1 desktop:mt-0 tablet:ml-1 hover:bg-navyBlue-100">
             Search
           </button>
