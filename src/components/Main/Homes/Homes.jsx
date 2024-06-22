baf866bcebef78c1f3289e75eac1ab82126c872d
Luna222 <lunameowth@gmail.com>
2024-06-22 20:36:33 +0700

    up

--- src/components/Main/Homes/Homes.jsx
+++ src/components/Main/Homes/Homes.jsx
@@ -1,7 +1,27 @@
+import HOTEL_DATA from '../../../../data/hotel_list.json';
+import HomeItem from './HomeItem';
+
 export default function Homes() {
   return (
     <section id="homes">
-      <article></article>
+      <div className="container-md pt-0">
+        <h2 className="mb-6">Home guests love</h2>
+        <div className="flex items-start overflow-x-auto">
+          {HOTEL_DATA.map(
+            ({ name, city, price, rate, type, image_url }, idx) => (
+              <HomeItem
+                key={idx}
+                name={name}
+                city={city}
+                price={price}
+                rate={rate}
+                type={type}
+                imageUrl={image_url}
+              />
+            )
+          )}
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Main/PropertyTypes/PropItem.jsx
+++ src/components/Main/PropertyTypes/PropItem.jsx
@@ -2,13 +2,13 @@ export default function PropItem(props) {
   return (
     <figure
       id="animated-property"
-      className="grow shrink-0 flex flex-col justify-start items-start space-y-1 group relative m-3 first-child:ml-0 last-child:mr-0 h-64 w-64 opacity-0 transform transition-all delay-150 duration-500 ease-out animate-fade-in-up"
+      className="grow shrink-0 flex flex-col justify-start items-start space-y-2 group relative m-3 first-child:ml-0 last-child:mr-0 opacity-0 transform transition-all delay-150 duration-500 ease-out animate-fade-in-up"
     >
       <div className="relative">
         <img
           src={props.image}
-          alt="Delightful City"
-          className="w-[256px] h-[200px] object-cover rounded-t-xl"
+          alt="Delightful Property"
+          className="grow w-[190px] h-[150px] object-cover rounded-t-xl"
         />
         <div className="overlay absolute p-6 inset-0 backdrop-brightness-100 backdrop-filter transition-all duration-200 ease-in-out group-hover:bg-white/50 group-hover:backdrop-brightness-105 rounded-t-xl"></div>
       </div>
