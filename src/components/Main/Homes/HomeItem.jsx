ef219b5b76ed9023981bc811527d14fd0505a170
Luna222 <lunameowth@gmail.com>
2024-06-22 20:48:28 +0700

    up

--- src/components/Main/Homes/HomeItem.jsx
+++ src/components/Main/Homes/HomeItem.jsx
@@ -2,13 +2,13 @@ export default function HomeItem(props) {
   return (
     <article
       id="animated-property"
-      className="grow shrink-0 flex flex-col justify-start items-start space-y-2 group relative m-3 first-child:ml-0 last-child:mr-0 opacity-0 transform transition-all delay-150 duration-500 ease-out animate-fade-in-up hover:border-2 hover:border-navyBlue-100"
+      className="grow shrink-0 flex flex-col justify-start items-start space-y-2 group relative m-3 first-child:ml-0 last-child:mr-0 opacity-0 transform transition-all delay-150 duration-500 ease-out animate-fade-in-up"
     >
       <div className="relative">
         <img
           src={props.imageUrl}
           alt="Delightful Hotel"
-          className="grow w-[242px] h-[270px] object-cover"
+          className="w-[242px] h-[270px] object-cover"
         />
         <div className="overlay absolute p-6 inset-0 backdrop-brightness-100 backdrop-filter transition-all duration-200 ease-in-out group-hover:bg-white/50 group-hover:backdrop-brightness-105"></div>
       </div>
