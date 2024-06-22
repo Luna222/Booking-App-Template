b0ad89bf388501d9039bd3373914e33e1abc2235
Luna222 <lunameowth@gmail.com>
2024-06-22 17:09:20 +0700

    up

--- src/components/Main/PropertyTypes/PropertyTypes.jsx
+++ src/components/Main/PropertyTypes/PropertyTypes.jsx
@@ -4,8 +4,8 @@ import PropItem from './PropItem';
 export default function PropertyTypes() {
   return (
     <section id="types">
-      <div className="container-md">
-        <p>Browse by property type</p>
+      <div className="container-md pt-0">
+        <h2 className="mb-6">Browse by property type</h2>
         <div className="flex items-start overflow-x-auto">
           {PROPERTY_DATA.map(({ name, count, image }, idx) => (
             <PropItem key={idx} name={name} count={count} image={image} />
