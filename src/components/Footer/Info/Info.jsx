dcbc9bd6e6fac0428c863c94e5bad56d0fb0ff05
Luna222 <lunameowth@gmail.com>
2024-06-24 14:11:41 +0700

    up

--- src/components/Footer/Info/Info.jsx
+++ src/components/Footer/Info/Info.jsx
@@ -1,11 +1,23 @@
 import FOOTER_DATA from '../../../../data/footer.json';
 
-function FooterItem(props) {
-  return <div></div>;
+function ListItem(props) {
+  return (
+    <ul className="space-y-2">
+      {props.values.map((item, idx) => (
+        <li key={idx} className="hover:underline">
+          <a href="#">{item}</a>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default function Info() {
   return (
-    <div className="grow grid grid-cols-2 tablet:grid-cols-3 desktop:grid-cols-5 p-6 text-navyBlue-300"></div>
+    <div className="container-md grid grid-cols-3 tablet:grid-cols-5 items-start gap-6 tablet:gap-10 text-navyBlue-300 text-sm text-wrap">
+      {FOOTER_DATA.map(({ col_number, col_values }) => (
+        <ListItem key={col_number} values={col_values} />
+      ))}
+    </div>
   );
 }
