2c8dcabd6be8133c7335932fadd648a175b89c7d
Luna222 <lunameowth@gmail.com>
2024-06-27 00:53:52 +0700

    up

--- src/components/SearchList/SearchListItem.jsx
+++ src/components/SearchList/SearchListItem.jsx
@@ -1,22 +1,40 @@
 export default function SearchListItem(props) {
   return (
-    <li className="group relative border-[1.5px] border-gray-300 p-3 rounded transition-all delay-150 duration-500 ease-out hover:scale-[.98]">
+    <li className="group relative border-[1.5px] border-gray-300 p-3 rounded transition-all delay-150 duration-500 ease-out hover:scale-[.98] text-wrap">
       <a href="#" className="flex justify-start items-start space-x-5">
         <div className="relative">
           <img
             src={props['image_url']}
             alt="Delightful Hotel"
-            className="w-[195px] h-[205px] tablet:w-[215px] tablet:h-[230px] object-cover"
+            className="w-[195px] h-[205px] tablet:w-[210px] tablet:h-[225px] object-cover"
           />
           <div className="overlay absolute p-6 inset-0 backdrop-brightness-100 backdrop-filter transition-all duration-200 ease-in-out group-hover:bg-white/50 group-hover:backdrop-brightness-105"></div>
         </div>
 
-        <div>
-          <div className="grow flex flex-col justify-start items-start space-y-2">
-            <div className="flex">
-              <h3>{props.name}</h3>
-              <p className="txt-label">{props['rate_text']}</p>
+        <div className="grow">
+          <div className="flex flex-col justify-start items-stretch space-y-1">
+            <div className="grow flex justify-start items-center space-x-20">
+              <h3 className="text-navyBlue-50/95">{props.name}</h3>
+              <p className="grow flex justify-between items-start">
+                <span className="txt-label">{props['rate_text']}</span>
+                <span className="text-white bg-navyBlue-300 p-1">
+                  {props.rate}
+                </span>
+              </p>
+              {/* <p className="grow ml-20 txt-label">{props['rate_text']}</p> */}
             </div>
+
+            <p></p>
+
+            <p></p>
+
+            <p></p>
+
+            <div className="flex"></div>
+
+            <div className="flex"></div>
+
+            <div className="flex"></div>
           </div>
         </div>
       </a>
